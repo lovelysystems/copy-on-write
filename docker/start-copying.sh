0808befd9cc23ec0dc2c292a92a5caf6ca1f15aa
lukas <lukas.kaufmann@lovelysystems.com>
2023-02-20 14:00:31 +0100

    preserve folder structure within mapped folders

--- docker/start-copying.sh
+++ docker/start-copying.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e
 #TODO if source env ends with /
-IFS=';' read -r -a pairs <<< "$FOLDER_PAIRS"
 
+IFS=';' read -r -a pairs <<< "$FOLDER_PAIRS"
 function getTargetFolder {
   pair=$1
   IFS=":" read -r -a pairing <<< "$pair"
@@ -15,17 +14,28 @@ function getSourceFolder {
   echo "${pairing[0]}"
 }
 
-#/opt/src/nested/second/
-
 function matchesPairing {
-  input_path=$1
-  source_folder=$2
+  inputFilePath=$1
+  sourceFolder=$2
 
-  if [[ $input_path == *$source_folder* ]] #for now basically tests for contains
+  if [[ $inputFilePath =~ .?$sourceFolder.? ]] #TODO verify that regex in source works
   then
     return 0
   fi
   return 1
+}
+
+function getTargetFileFolders {
+  directoryOfFile=$1
+  fileName=$2
+  matchedSourceFolder=$3
+
+  rest=${directoryOfFile#*"$matchedSourceFolder"}
+  if [[ ${rest: -1} == "/" ]]
+  then
+      rest=${rest:0:-1}
+  fi
+  echo "$rest"
 }
 
 function getOutputPath {
@@ -34,36 +44,45 @@ function getOutputPath {
 
   for p in "${pairs[@]}";
   do
-    source_folder=$(getSourceFolder $p)
-    #echo "checking $inputDir $TARGET_ROOT$source_folder"
-    if matchesPairing "$inputDir" "$source_folder";
+    sourceFolder=$(getSourceFolder "$p")
+    #echo "checking $inputDir $TARGET_ROOT$sourceFolder"
+    if matchesPairing "$inputDir$inputFileName" "$sourceFolder";
     then
-      #echo "matched $TARGET_ROOT$source_folder"
-      target_folder=$(getTargetFolder $p)
-      echo "$TARGET_ROOT$target_folder/$inputFileName"
+      targetFolder=$(getTargetFolder "$p")
+      targetFileFolders=$(getTargetFileFolders "$inputDir" "$inputFileName" "$sourceFolder")
+      echo "$TARGET_ROOT$targetFolder$targetFileFolders/$inputFileName"
       return 0
     fi
   done
-  echo "$TARGET_ROOT$inputFileName" #fallback
-  return 0
 }
 
 #create target folders
 for p in "${pairs[@]}";
 do
-  target_folder=$(getTargetFolder $p)
-  target_path="$TARGET_ROOT$target_folder"
-  echo "Ensuring directory $target_path exists"
-  mkdir -p "$target_path"
+  targetFolder=$(getTargetFolder $p)
+  targetPath="$TARGET_ROOT$targetFolder"
+  echo "Ensuring directory $targetPath exists"
+  mkdir -p "$targetPath"
 done
 
-
-inotifywait -r -m $SOURCE_ROOT -e create -e moved_to |
+inotifywait -mr "$SOURCE_ROOT" -e create -e moved_to |
   while read -r dir action file; do
-    echo "The file '$file' appeared in directiory '$dir' via '$action'"
-    full_source_path="$dir$file"
+    fullSourcePath="$dir$file"
     echo "getting output for $dir$file"
-    target_path=$(getOutputPath $dir $file)
-    echo "copying to $target_path"
-    cp -p $full_source_path $target_path
+    targetPath=$(getOutputPath "$dir" "$file")
+
+    #TODO if new folder is read, reestablish listeners for that folder
+    if [[ -z "$targetPath" ]];
+    then
+      echo "No matching pair found. Ignoring"
+    else
+      if [[ -f $fullSourcePath ]]; then
+        mkdir -p "$(dirname "$targetPath")"
+        echo "copying to $targetPath"
+        cp -p "$fullSourcePath" "$targetPath"
+      elif [[ -d $fullSourcePath ]]; then
+        echo "creating directory $fullSourcePath"
+        mkdir -p "$fullSourcePath"
+      fi
+    fi
   done
