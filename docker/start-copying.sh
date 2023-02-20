50c13233a6b7be1c242a4b6b7ef3af9ee0adf269
lukas <lukas.kaufmann@lovelysystems.com>
2023-02-20 16:59:11 +0100

    fixed copying of files in newly created folders

--- docker/start-copying.sh
+++ docker/start-copying.sh
@@ -65,31 +65,22 @@ do
   mkdir -p "$targetPath"
 done
 
-function fileEventHandling {
-  dir=$1
-  action=$2
-  file=$3
-}
-
 inotifywait -mr "$SOURCE_ROOT" -e moved_to,create |
   while read -r dir action file; do
-    echo "$action $dir$file"
     fullSourcePath="$dir$file"
-    echo "getting output for $dir$file"
-    targetPath=$(getOutputPath "$dir" "$file")
+      echo "getting output for $dir$file"
+      targetPath=$(getOutputPath "$dir" "$file")
 
-    #TODO if new folder is read, make sure those folders are also watched for that folder
-    if [[ -z "$targetPath" ]];
-    then
-      echo "No matching pair found. Ignoring"
-    else
-      if [[ -f $fullSourcePath ]]; then
-        mkdir -p "$(dirname "$targetPath")"
-        echo "copying to $targetPath"
-        cp -p "$fullSourcePath" "$targetPath"
-      elif [[ -d $fullSourcePath ]]; then
-        inotifywait -mr "$fullSourcePath" -e moved_to,create | /dev/null
-        echo "watch for $fullSourcePath created (hopefully)"
+      if [[ -z "$targetPath" ]];
+      then
+        echo "No matching pair found. Ignoring"
+      else
+        if [[ -f $fullSourcePath ]]; then
+          mkdir -p "$(dirname "$targetPath")"
+          echo "copying to $targetPath"
+          cp -p "$fullSourcePath" "$targetPath"
+        elif [[ -d $fullSourcePath ]]; then
+            :
+        fi
       fi
-    fi
   done
